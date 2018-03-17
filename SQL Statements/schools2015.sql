7603e0cbcce1560574524a851999d85dcd4da245
campincarl619 <joshneal619@gmail.com>
2018-03-17 10:30:01 -0700

    Add files via upload

---
+++ SQL Statements/schools2015.sql
@@ -0,0 +1,20 @@
+SET @row_number = 10000;
+
+CREATE TABLE schooldata.schools2015
+AS
+SELECT
+     concat((@row_number:=@row_number + 1),"A") AS Shit
+    ,poop.Institution_Name
+    ,poop.Region_Name
+    ,poop.Year
+    ,poop.SAT_Avg
+    ,poop.Admission_Rate
+    ,poop.Tuition_Cost
+    ,poop.Latitude
+    ,poop.Longitude
+FROM schooldata.school_data_all poop
+WHERE Year = 2014
+    AND (poop.SAT_Avg != -1
+                AND poop.Admission_Rate != -1
+                AND poop.Tuition_Cost != -1)
+                ORDER BY SAT_Avg desc;
